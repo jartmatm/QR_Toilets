1276bcf29267d72bac055efc2648ff16a2811a4d
Jairo Mateus <jartmatm@gmail.com>
2025-08-14 11:57:05 +1000

    Update QR_grafitti_pipeline.py

--- QR_grafitti_pipeline.py
+++ QR_grafitti_pipeline.py
@@ -6,8 +6,8 @@ from funciones import (load_file, save_file, Apply_Shift, input_date)
 df = load_file()
 df = df.drop(COLUMNAS_ELIMINAR_GRAFITTI, axis=1)
 df = df.rename(columns=COLUMNAS_RENAME_GRAFITTI)
-df['Start time'] = pd.to_datetime(df['Start time'], format='%d/%m/%Y %H:%M:%S') #convertir formato datetime
-df['Completion time'] = pd.to_datetime(df['Completion time'], format='%d/%m/%Y %H:%M:%S') #convertir formato datetime
+df['Start time'] = pd.to_datetime(df['Start time'], format='%d/%m/%Y %H:%M:%S') 
+df['Completion time'] = pd.to_datetime(df['Completion time'], format='%d/%m/%Y %H:%M:%S') 
 df['Shift'] = df['Start time'].dt.hour.apply(Apply_Shift)
 df['Reporter Name'] = df['Reporter Name'].str.split().str[0]
 df['Status'] = df['Status'].str.split('-').str[-1]
@@ -21,5 +21,6 @@ for col in COLUMNS_TO_CAPITALIZE_GRAFITTI:
 
 INICIO, FIN = input_date()
 df = df[(df['Start time'] >= INICIO) & (df['Start time'] <= FIN)]
+
 
 save_file(df)
